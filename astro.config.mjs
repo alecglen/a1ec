9b24320eed125602e9855b3503a1bceebd0dd073
Alec Ostrander <alec.ostrander@gmail.com>
2025-10-29 15:30:44 +0000

    cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import preact from '@astrojs/preact';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://a1ec.dev',
+  site: "https://a1ec.dev",
   server: { host: true },
-  integrations: [preact()]
+  integrations: [],
 });
